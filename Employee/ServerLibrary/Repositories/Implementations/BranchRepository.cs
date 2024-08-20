4842251586a23f7eeb3a0526581c5a1a58d7ad1e
huynhlethanhnhan <thanhnhanhuynh1807@gmail.com>
2024-08-21 05:02:22 +0700

    Update General Department/Country

--- Employee/ServerLibrary/Repositories/Implementations/BranchRepository.cs
+++ Employee/ServerLibrary/Repositories/Implementations/BranchRepository.cs
@@ -19,13 +19,17 @@ namespace ServerLibrary.Repositories.Implementations
             return Success();
         }
 
-        public async Task<List<Branch>> GetAll() => await appDbContext.Branches.ToListAsync();
+        public async Task<List<Branch>> GetAll() => await appDbContext
+            .Branches
+            .AsNoTracking()
+            .Include(d=>d.Department)
+            .ToListAsync();
 
         public async Task<Branch> GetById(int id) => await appDbContext.Branches.FindAsync(id);
 
         public async Task<GeneralResponse> Insert(Branch item)
         {
-            if (!await CheckName(item.Name!)) return new GeneralResponse(false, "Department already added");
+            if (!await CheckName(item.Name!)) return new GeneralResponse(false, "Branch already added");
             appDbContext.Branches.Add(item);
             await Commit();
             return Success();
@@ -33,14 +37,15 @@ namespace ServerLibrary.Repositories.Implementations
 
         public async Task<GeneralResponse> Update(Branch item)
         {
-            var dep = await appDbContext.Branches.FindAsync(item.Id);
-            if (dep is null) return NotFound();
-            dep.Name = item.Name;
+            var branch = await appDbContext.Branches.FindAsync(item.Id);
+            if (branch is null) return NotFound();
+            branch.Name = item.Name;
+            branch.DepartmentId = item.DepartmentId;
             await Commit();
             return Success();
         }
 
-        private static GeneralResponse NotFound() => new(false, "Sorry department not found");
+        private static GeneralResponse NotFound() => new(false, "Sorry branch not found");
 
         private static GeneralResponse Success() => new(true, "Process completed");
 
