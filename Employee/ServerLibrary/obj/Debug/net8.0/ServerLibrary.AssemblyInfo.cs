7e3843320d06f998b923e9978e307ca34aa7d9d8
huynhlethanhnhan <thanhnhanhuynh1807@gmail.com>
2024-08-29 16:16:56 +0700

    Fix Complete

--- Employee/ServerLibrary/obj/Debug/net8.0/ServerLibrary.AssemblyInfo.cs
+++ Employee/ServerLibrary/obj/Debug/net8.0/ServerLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b7cbeea7585224b1efc24e8ab51524e55aa6bdb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0ac78c6f3cd17d67fe1cf31e0cb8d02882edfbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
